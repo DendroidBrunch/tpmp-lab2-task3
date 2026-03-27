9e44f21ab1d90da16ed08446e95e99bd37dd6418
dd_brnch <barys.zhylko@gmail.com>
2026-03-27 08:42:51 +0300

    output.h

---
+++ src/output.h
@@ -0,0 +1,9 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include "student.h"
+
+void print_high_average(STUD *students, int count, float min);
+void print_all_students(STUD *students, int count);
+
+#endif
