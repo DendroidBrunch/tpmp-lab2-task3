c36c0da5ec0b3530d01a8c4a49be4df869cb2a77
dd_brnch <barys.zhylko@gmail.com>
2026-03-27 08:44:15 +0300

    utils.h

---
+++ src/utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "student.h"
+
+void sort_students_by_name(STUD *students, int count);
+int count_high_average(STUD *students, int count, float threshold);
+
+#endif
